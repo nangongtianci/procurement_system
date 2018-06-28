7d31b50e93e31cc174d6e444f21e45a0591f81ee
nangongtianci <675886926@qq.com>
2018-06-28 10:01:52 +0800

    修改ssl配置类，重定向80端口到443端口

--- project-parent/procurement/src/main/java/com/personal/config/web/SSLConfig.java
+++ project-parent/procurement/src/main/java/com/personal/config/web/SSLConfig.java
@@ -31,7 +31,7 @@ public class SSLConfig {
     private Connector initiateHttpConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(8080);
+        connector.setPort(80);
         connector.setSecure(false);
         connector.setRedirectPort(8443);
         return connector;
