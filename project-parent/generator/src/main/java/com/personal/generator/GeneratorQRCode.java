cde152672158d40fafb7095a5440c2eedb39ecf8
nangongtianci <675886926@qq.com>
2018-07-15 09:26:30 +0800

    账单应收查询返回账单主键字段

--- project-parent/generator/src/main/java/com/personal/generator/GeneratorQRCode.java
+++ project-parent/generator/src/main/java/com/personal/generator/GeneratorQRCode.java
@@ -49,7 +49,7 @@ public class GeneratorQRCode {
         File logoFile = new File(path+"shop.png");
         File QrCodeFile = new File(path+"logo_code.png");
         File QrCodeFile2 = new File(path+"code.png");
-        String url = "https://www.baidu.com/";
+        String url = "https://www.bequickers.com/static/img/";
         drawLogoQRCode(logoFile, QrCodeFile, url, null, "png");
         drawLogoQRCode(null, QrCodeFile2, url, null, "png");
     }
