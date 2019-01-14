b7aa4d0c3c0084eaa4a4b2b7ef7f3efa5590d96a
nangongtianci <675886926@qq.com>
2019-01-14 16:49:00 +0800

    微小本后台初始化

---
+++ project-parent/msb-admin/src/main/java/com/msb/MsbAdminApplication.java
@@ -0,0 +1,14 @@
+package com.msb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsbAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsbAdminApplication.class, args);
+	}
+
+}
+
